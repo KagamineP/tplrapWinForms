76d03f6d014d09fe07b5682f9c891b24e680b7bc
KagamineP <kagaminep@kagaminep.ru>
2019-08-15 17:07:17 +0500

    Brand new update

---
+++ README.md
@@ -0,0 +1,17 @@
+# TP-Link Router Administration Panel
+
+This program was created to simplify the login to the admin panel on [TP-Link](https://tp-link.com/) routers. This program requires a connection to the router via Wi-Fi. This program uses [CefSharp](https://github.com/cefsharp/CefSharp), so the program loads the page http://tplinkwifi.net/ (works only on TP-Link routers). Ie, this program is a shortcut to access the administration panel.
+
+System requirements
+==================
+Operating system from Windows 7 to Windows 10  
+.Net Framework 4.7.2
+
+Build dependencies
+==================
+To build this program you need to install CefSharp from NuGet.
+
+Copyright
+=========
+TP-Link Brand is owned by TP-LINK Technologies CO., LTD.  
+I own only the program code.
---
+++ TPLRAP/frmMain.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using CefSharp;
+using CefSharp.WinForms;
+using CefSharp.WinForms.Internals;
+using AutoUpdaterDotNET;
+
+namespace TPLRAP
+{
+    public partial class FrmMain : Form
+    {
+        public ChromiumWebBrowser chromeBrowser;
+
+        public FrmMain()
+        {
+#if DEBUG
+            MessageBox.Show(Properties.Resources.DebugMessageText, Properties.Resources.DebugMessageTitle, MessageBoxButtons.OK, MessageBoxIcon.Information);
+            MessageBox.Show("CefSharp version: " + AssemblyInfo.AssemblyVersion, "Debug configuration", MessageBoxButtons.OK, MessageBoxIcon.Information);
+#endif
+            InitializeComponent();
+            InitializeChromium();
+        }
+
+        #region All, what connected with CefSharp settings
+        public void InitializeChromium()
+        {
+            CefSettings settings = new CefSettings();
+            Cef.Initialize(settings);
+            chromeBrowser = new ChromiumWebBrowser("http://tplinkwifi.net/");
+            this.Controls.Add(chromeBrowser);
+            chromeBrowser.Dock = DockStyle.Fill;
+            chromeBrowser.MenuHandler = new menuHandler();
+        }
+        #endregion
+
+
+        #region Form closing
+        protected override void OnFormClosing(FormClosingEventArgs e)
+        {
+            if (CloseCancel()==false)
+            {
+                e.Cancel = true;
+            };
+        }
+
+        public static bool CloseCancel()
+        {
+            var result = MessageBox.Show(Properties.Resources.ConfirmExitMessage, Properties.Resources.DebugMessageTitle,
+                MessageBoxButtons.YesNo,
+                MessageBoxIcon.Question);
+
+            if (result == DialogResult.Yes)
+                return true;
+            else
+                return false;
+        }
+
+        #endregion
+
+        #region AutoUpdater
+        private void FrmMain_Load(object sender, EventArgs e)
+        {
+            AutoUpdater.Start("http://kagaminep.ru/tplrap/updates/AutoUpdaterTest.xml");
+        }
+        #endregion
+    }
+}
